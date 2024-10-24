aa0c69d048d40e465378ca7cca20a822e8567c55
Fingolfin1001 <jama8933@colorado.edu>
2024-10-24 11:05:45 -0600

    Update Lab13.py

--- Lab13.py
+++ Lab13.py
@@ -1,5 +1,5 @@
 # ECEN 2440
-# James Mathews
+# Group 12
 # Lab 13
 # 10/24/2024
     
@@ -21,6 +21,9 @@ pwm_rate = 2000
 ain1_ph = Pin(12, Pin.OUT)
 ain2_en = PWM(13, freq = pwm_rate, duty_u16 = 0)
 
+bin1_ph = Pin(10, Pin.OUT)
+bin2_en = PWM(11, freq = pwm_rate, duty_u16 = 0)
+
 pwm = min(max(int(2**17 * abs(1)), 0), 65535)
 
 # Callback function to execute when an IR code is received
@@ -28,23 +31,54 @@ def ir_callback(data, addr, _):
     if (data == 1):
         print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
 
-        print("Motor B Forward")
+        print("Motor A Forward")
         ain1_ph.low()
         ain2_en.duty_u16(pwm)
-        time.sleep(2)
+        time.sleep(1)
+
+        print("Motor B Forward")
+        bin1_ph.low()
+        bin2_en.duty_u16(pwm)
+        time.sleep(1)
 
     elif (data == 2):
         print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
+
+        print("Motor A Stopped")
+        ain1_ph.low()
+        ain2_en.duty_u16(0)
+        time.sleep(1)
+
+        print("Motor B Stopped")
+        bin1_ph.low()
+        bin2_en.duty_u16(0)
+        time.sleep(1)
+
     elif (data == 3):
         print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
 
-        print("Motor B Reverse")
+        print("Motor A Reverse")
         ain1_ph.high()
         ain2_en.duty_u16(pwm)
-        time.sleep(2)
+        time.sleep(1)
+
+        print("Motor B Reverse")
+        bin1_ph.high()
+        bin2_en.duty_u16(pwm)
+        time.sleep(1)
 
     elif (data == 4):
         print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
+
+        print("Motor A Stopped")
+        ain1_ph.low()
+        ain2_en.duty_u16(0)
+        time.sleep(1)
+
+        print("Motor B Stopped")
+        bin1_ph.low()
+        bin2_en.duty_u16(0)
+        time.sleep(1)
 
 # Setup the IR receiver
 ir_pin = Pin(17, Pin.IN, Pin.PULL_UP) # Adjust the pin number based on your wiring
